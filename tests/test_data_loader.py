f172fa8b877fe11bce478d863ae692781dd62ed7
zhl24vnc <zhl24vnc@bangor.ac.uk>
2024-10-24 20:03:59 +0100

    Update test_data_loader.py

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -3,12 +3,9 @@ import pandas as pd
 from titanic_analysis.data_loader import load_titanic_data
 
 def test_load_titanic_data():
-    # 获取当前文件的目录
+ 
     current_dir = os.path.dirname(__file__)
-    # 构建数据文件的绝对路径
     data_path = os.path.join(current_dir, '..', 'data', 'titanic.csv')
-    
-    # 打印路径以进行调试
     print("Data file path:", os.path.abspath(data_path))
     
     df = load_titanic_data(data_path)
