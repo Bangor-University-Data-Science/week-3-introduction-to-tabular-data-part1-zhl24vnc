7c06173db27453f5ce13420cc850e334efe0ab0b
zhl24vnc <zhl24vnc@bangor.ac.uk>
2024-10-24 19:01:26 +0100

    Update data_loader.py

--- titanic_analysis/data_loader.py
+++ titanic_analysis/data_loader.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 def load_titanic_data(filepath: str) -> pd.DataFrame:
     """Load the Titanic dataset from a CSV file."""
-    return pd.read_csv(filepath)
+return pd.titanic_csv(filepath)
